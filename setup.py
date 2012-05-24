b044efd84b57aafdb279cc7bd0ee778ac83024b7
Christopher Granade <cgranade@cgranade.com>
2012-05-24 13:07:11 -0400

    Added metadata to setup.py in preparation for PyPI submission.

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 
 setup(
     name='QuaEC',
-    version='0.1alpha1',
+    version='0.1a1',
+    url='http://cgranade.github.com/python-quaec/',
+    author='Chris Granade and Ben Criger',
+    author_email='cgranade@cgranade.com',
     package_dir={'': 'src'},
     packages=['qecc']
 )
