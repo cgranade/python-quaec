aa4f5229be6dc7eb66af309cb53eb25f06f17bd9
Ben Criger <bcriger@gmail.com>
2012-10-12 12:46:32 -0400

    Fixed copy-paste error in documentation for solve_commutation_constraints

--- src/qecc/constraint_solvers.py
+++ src/qecc/constraint_solvers.py
@@ -51,7 +51,7 @@ def solve_commutation_constraints(
     :param commutation_constraints: A list of operators :math:`\{A_i\}` such
         that each solution :math:`P` yielded by this function must satisfy
         :math:`[A_i, P] = 0` for all :math:`i`.
-    :param commutation_constraints: A list of operators :math:`\{B_i\}` such
+    :param anticommutation_constraints: A list of operators :math:`\{B_i\}` such
         that each solution :math:`P` yielded by this function must satisfy
         :math:`\{B_i, P\} = 0` for all :math:`i`.
     :param search_in_gens: A list of operators :math:`\{N_i\}` that generate
