b00686f0e5cf1f6643ed478cd231c8129439a055
bcriger <bcriger@gmail.com>
2014-12-08 14:09:10 +0100

    Fixed list comprehension (dumb)

--- src/qecc/CliffordClass.py
+++ src/qecc/CliffordClass.py
@@ -314,7 +314,9 @@ class Clifford(object):
         new_bsv = self._bsm * other._bsv
         xz_vec = other._bsv._x + other._bsv._z
         new_bsm_phase = other._bsm_phase + \
-            sum([self.phase_vec[idx] if xz_vec[idx]])
+            sum([self.phase_vec[idx] 
+                for idx in range(len(xz_vec)) 
+                if xz_vec[idx]])
 
         return Pauli(_bsv = new_bsv, _bsm_phase = new_bsm_phase)
         
